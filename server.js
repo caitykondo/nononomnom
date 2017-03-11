2adddb2fcb50fce1158232576429707fc3548e94
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-03-10 23:25:14 -1000

    add bodyParser package

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 const food = require('./routes/food');
 
 app.use(express.static('/'));
 
-app.use('/food', food);
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+app.post('/', (req, res) => {
+  res.send(req.body);
+});
+
+app.use('/food', food);
 
 app.listen( PORT, ()=>{
   console.log('listening on', PORT);
