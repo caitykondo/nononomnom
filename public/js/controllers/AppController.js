bb4a5d7c6ab7026959a7c82d6627f4c66816511e
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-03-11 15:44:07 -1000

    use food.json for sudo autocomplete

--- public/js/controllers/AppController.js
+++ public/js/controllers/AppController.js
@@ -1,6 +1,8 @@
 myApp.controller('AppController', [
-  '$scope', 'CheckDiet',
-  function ($scope, CheckDiet) {
+  '$scope', 'CheckDiet', 'FoodDataFactory',
+  function ($scope, CheckDiet, FoodDataFactory) {
+    console.log(FoodDataFactory);
+    $scope.foodData = FoodDataFactory;
     $scope.CheckDiet = CheckDiet;
     $scope.dietList = ["low carb", "paleo", "vegan", "vegetarian", "keto"];
     $scope.foodDataIpsum = [
